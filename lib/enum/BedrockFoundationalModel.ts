4d18098b2bb051d4c355bc90ce8ccc1dfbeaffa4
Pratham Sharma <35078777+Pratham1807@users.noreply.github.com>
2024-10-29 14:28:14 +0530

    Create BedrockFoundationalModel.ts

---
+++ lib/enum/BedrockFoundationalModel.ts
@@ -0,0 +1,4 @@
+export enum BedrockFoundationModel {
+  ANTHROPIC_CLAUDE_3_HAIKU = 'anthropic.claude-3-haiku-20240307-v1:0',
+  ANTHROPIC_CLAUDE_3_SONNET = 'anthropic.claude-3-sonnet-20240229-v1:0',
+}
